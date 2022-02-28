9b0ac652d82418266d07eb3149c4bbc270468948
John Tomlinson <jtomlinson@brightree.com>
2022-02-28 09:37:48 +0000

    Created Image Printed Text Recognition Tester console app

---
+++ ImagePrintedTextRecognitionTesterApp/Program.cs
@@ -0,0 +1,51 @@
+﻿using ImagePrintedTextRecognitionShared;
+using ImageRecognitionEngine;
+using Microsoft.Extensions.Configuration;
+
+try
+{
+    var builder = new ConfigurationBuilder()
+                               .SetBasePath($"{Directory.GetCurrentDirectory()}/../../..")
+                               .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+
+    var config = builder.Build();
+
+    using (var fileStream = new FileStream(config["ImageFilePath"], FileMode.Open, FileAccess.Read))
+    {
+        var imageRecognitionInput = new ImageRecognitionInput()
+        {
+            SubscriptionKey = config["AzureSubscriptionKey"],
+            AzureEndpointURL = config["AzureEndpointURL"],
+            UploadImageFileStream = fileStream,
+        };
+
+        var imagePrintedTextRecognitionService = new ImagePrintedTextRecognitionService();
+        var imageRecognitionOutput = await imagePrintedTextRecognitionService.UploadFileAndConvertToText(imageRecognitionInput);
+
+        Console.WriteLine("Printed text in image: \n");
+
+        if (imageRecognitionOutput != null)
+        {
+            if (!string.IsNullOrWhiteSpace(imageRecognitionOutput.ErrorMessage))
+            {
+                Console.WriteLine($"Error: {imageRecognitionOutput.ErrorMessage}");
+            }
+            else if (!string.IsNullOrWhiteSpace(imageRecognitionOutput.PrintedTextInImage))
+            {
+                Console.WriteLine(imageRecognitionOutput.PrintedTextInImage);
+            }
+            else
+            {
+                Console.WriteLine("No printed text found in image file");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Printed text in image file could not be found");
+        }
+    }
+}
+catch (FileNotFoundException ioEx)
+{
+    Console.WriteLine(ioEx.Message);
+}
